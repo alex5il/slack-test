47be477255cfd10290ecdd5e4964d4b4c263351c
alex5il <alexko@wix.com>
2018-07-08 13:22:19 +0300

    first comm

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import express from 'express';
 import { log } from './utils';
 import { reportsList, generateReport } from './modules/reports';
 
+const Slack = require('slack-node');
+
 const router = new express.Router();
 
 router.post('/slack/events/eransh', async (req, res) => {
@@ -70,6 +72,21 @@ router.post('/slack/command/getReportFile', async (req, res) => {
   }
 });
 
+function sendMsg(channel, text, slack) {
+  try {
+    slack.api('chat.postMessage', {
+      text,
+      channel,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function getCommitMessage(commit) {
+  return `Hey! ${commit.committer.name} is in need of a code review, Are you brave enough?\n${commit.url}`;
+}
+
 router.post('/slack/actions', async (req, res) => {
   try {
     const slackReqObj = JSON.parse(req.body.payload);
@@ -84,6 +101,30 @@ router.post('/slack/actions', async (req, res) => {
     log.error(err);
     return res.status(500).send('Something blew up. We\'re looking into it.');
   }
+});
+
+router.all('/slack/actions/nachschons', async (req, res) => {
+
+  const slack = new Slack('xoxp-360464771431-360309249750-394514050788-54bfa811243b08d20fc7a57b17e20d16');
+
+  req.body.commits.forEach((commit) => {
+    if (/(^|\|)client-review(\||$)/.test(commit.message)) {
+      sendMsg('#client', getCommitMessage(commit), slack);
+      console.log('client - message :: ', commit.message);
+      console.log('client :: ', commit.url);
+    } else if (/(^|\|)server-review(\||$)/.test(commit.message)) {
+      sendMsg('#server', getCommitMessage(commit), slack);
+      console.log('server - message :: ', commit.message);
+      console.log('server :: ', commit.url);
+    } else {
+      console.log('message :: ', commit.message);
+      sendMsg('#server', `${commit.committer.name} forgot to code review, let's all laugh at him :rolling_on_the_floor_laughing: `, slack);
+      sendMsg('#client', `${commit.committer.name} forgot to code review, let's all laugh at him :rolling_on_the_floor_laughing: `, slack);
+      console.log('ignore :: ', commit.message);
+    }
+  });
+
+  return res.status(200).send('hell yeah');
 });
 
 export default router;
